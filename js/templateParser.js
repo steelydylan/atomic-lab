4ddb7163ffef9745fb95cb2961b32a277cb041b8
steelydylan <ess_president@me.com>
2016-07-14 20:04:17 +0900

    test

--- js/templateParser.js
+++ js/templateParser.js
@@ -1,10 +1,11 @@
 (function(global){
 
 	var parseTemplate = function(text){
+		console.log(text.match(/<!-- preview -->(.*)<!-- \/preview -->/g));
 		return text;
 	};
 
 	global.parseTemplate = parseTemplate;
 })(window);
 
-console.log(parseTemplate('<!-- template modifier="" text="button" --><button class="button {modifiler}">{text}</button><!-- /template --><!-- preview --><!-- Button modifier="" text="default" --><!-- Button modifier="button-red" text="red" --><!-- /preview -->'));
+parseTemplate('<!-- template modifier="" text="button" --><button class="button {modifiler}">{text}</button><!-- /template --><!-- preview --><!-- Button modifier="" text="default" --><!-- Button modifier="button-red" text="red" --><!-- /preview -->');
