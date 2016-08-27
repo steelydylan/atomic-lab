7a2765ffed673132dc015245a2ebb4d442ff2c5e
Takenori Okada <okajax@gmail.com>
2016-08-27 11:45:41 +0900

    SCSS導入

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,51 @@ var jshint = require('gulp-jshint');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-gulp.task('js',function(){
-	gulp.src(['js/src/main.js'])
-		.pipe(plumber({
-			handleError: function (err) {
-				console.log(err);
-				this.emit('end');
-			}
-		}))
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(browserify())
-		.pipe(gulp.dest('js/dist'))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('js/dist'))
-		.pipe(reload({stream:true}))
-		.pipe(notify('js task finished'))
+
+gulp.task('sass', function () {
+    gulp.src(['css/src/**/*.scss'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .pipe(gulp.dest('css/'));
 });
-gulp.task('default',function(){
-	browserSync.init({
-		server: "./"
-  });
-	gulp.watch('js/src/**/*.js',['js']);
+
+gulp.task('js', function () {
+    gulp.src(['js/src/main.js'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(browserify())
+        .pipe(gulp.dest('js/dist'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/dist'))
+        .pipe(reload({
+            stream: true
+        }))
+        .pipe(notify('js task finished'))
+});
+gulp.task('default', function () {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('js/src/**/*.js', ['js']);
+    gulp.watch('css/src/**/*.scss', ['sass']);
+});
+gulp.task('sass-watch', function () {
+    gulp.watch('css/src/**/*.scss', ['sass']);
 });
