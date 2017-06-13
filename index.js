c973ad685f3b9839fcbb5a5037c7b23c7ebdcd38
steelydylan <ess_president@me.com>
2017-06-13 22:09:15 +0900

    added eslint

--- index.js
+++ index.js
@@ -1,174 +1,171 @@
-var path = require('path');
-var atomicBuilder = {};
-var find = require('findit');
-var Promise = require('promise');
-var fs = require('fs-extra');
-var process_path = process.cwd();
-var mkdirp = require('mkdirp');
-var extend = require('extend');
-var getDirName = path.dirname;
+const path = require('path');
+const find = require('findit');
+const Promise = require('promise');
+const fs = require('fs-extra');
+const mkdirp = require('mkdirp');
+const extend = require('extend');
 
-var getFileInfo = function (dir) {
-  return new Promise(function (resolve, reject) {
-    var finder = find(dir);
-    var files = [];
-    finder.on('file', function (path, stat) {
-      files.push(path);
-    });
-    finder.on('end', function () {
-      resolve(files);
-    });
+const atomicBuilder = {};
+const getDirName = path.dirname;
+const processPath = process.cwd();
+
+const getFileInfo = dir => new Promise((resolve) => {
+  const finder = find(dir);
+  const files = [];
+  finder.on('file', (filePath) => {
+    files.push(filePath);
   });
-};
+  finder.on('end', () => {
+    resolve(files);
+  });
+});
 
-var getDoc = function (source, parser) {
-  var doc = source.match(parser.body);
+const getDoc = (source, parser) => {
+  let doc = source.match(parser.body);
   if (!doc) {
-    return "";
+    return '';
   }
   doc = doc[0];
   return doc
-    .replace(parser.start, "")
-    .replace(parser.end, "");
-}
+    .replace(parser.start, '')
+    .replace(parser.end, '');
+};
 
-var getMark = function (mark, source) {
-  var pattern = '@' + mark + '(?:[\t 　]+)(.*)';
-  var regex = new RegExp(pattern, "i");
-  var match = source.match(regex);
+const getMark = (mark, source) => {
+  const pattern = `@${mark}(?:[\t  ]+)(.*)`;
+  const regex = new RegExp(pattern, 'i');
+  const match = source.match(regex);
   if (match && match[1]) {
     return match[1];
-  } else {
-    return "";
   }
-}
+  return '';
+};
 
-var makeAtomicArray = function (files, markup, styling, parser) {
-  var components = [];
-  var id = 0;
-  for (var i = 0, n = files.length; i < n; i++) {
-    var file = files[i];
-    var paths = file.split("/");
-    var length = paths.length
-    var extName = path.extname(file).replace(".", "");
-    var baseName = path.basename(file, "." + markup);
+const makeAtomicArray = (files, markup, styling, parser) => {
+  const components = [];
+  let id = 0;
+  for (let i = 0, n = files.length; i < n; i += 1) {
+    const file = files[i];
+    const extName = path.extname(file).replace('.', '');
     if (extName !== markup) {
       continue;
     }
-    var html = fs.readFileSync(file, 'utf8');
-    var css, js, category, name, cssMark;
-    var doc = getDoc(html, parser);
+    const html = fs.readFileSync(file, 'utf8');
+    let css;
+    let js;
+    const doc = getDoc(html, parser);
     if (!doc) {
       continue;
     }
-    name = getMark("name", doc);
+    const name = getMark('name', doc);
     if (!name) {
       continue;
     }
-    category = getMark("category", doc) || "atom";
-    cssMark = getMark("css", doc);
+    const category = getMark('category', doc) || 'atom';
+    const cssMark = getMark('css', doc);
     if (cssMark) {
-      var cssPath = path.resolve(file, '../', cssMark);
+      const cssPath = path.resolve(file, '../', cssMark);
       try {
         css = fs.readFileSync(cssPath, 'utf8');
       } catch (err) {
         console.log(err);
       }
     }
-    var jsMark = getMark("js", doc);
+    const jsMark = getMark('js', doc);
     if (jsMark) {
-      var jsPath = path.resolve(file, '../', jsMark);
+      const jsPath = path.resolve(file, '../', jsMark);
       try {
         js = fs.readFileSync(jsPath, 'utf8');
       } catch (err) {
         console.log(err);
       }
     }
-    id++;
-    components.push({ category: category, name: name, html: html, css: css, js: js, itemId: id });
+    id += 1;
+    components.push({ category, name, html, css, js, itemId: id });
   }
   return components;
-}
+};
 
-var writeFile = function (path, contents, cb) {
-  mkdirp(getDirName(path), function (err) {
-    if (err) return cb(err);
-
-    fs.writeFile(path, contents, cb);
+const writeFile = function (filePath, contents, cb) {
+  mkdirp(getDirName(filePath), (err) => {
+    if (err) {
+      return cb(err);
+    }
+    return fs.writeFile(filePath, contents, cb);
   });
-}
+};
 
 atomicBuilder.build = function (opt) {
-  var src = opt.src;
-  var dist = opt.dist;
-  var markup = opt.markup;
-  var styling = opt.styling;
-  var parser = extend({
+  const src = opt.src;
+  const dist = opt.dist;
+  const markup = opt.markup;
+  const styling = opt.styling;
+  const parser = extend({
     start: /<!--@doc/g,
     end: /-->/g,
     body: /<!--@doc(([\n\r\t]|.)*?)-->/g
   }, opt.parser);
-  return getFileInfo(path.resolve(process_path, src))
-    .then(function (files) {
-      var components = makeAtomicArray(files, markup, styling, parser);
-      var json = JSON.stringify({ components: components });
-      var pjson = new Promise(function (resolve, reject) {
-        writeFile(path.resolve(process_path, dist), json, function (err) {
+  return getFileInfo(path.resolve(processPath, src))
+    .then((files) => {
+      const components = makeAtomicArray(files, markup, styling, parser);
+      const json = JSON.stringify({ components });
+      const pjson = new Promise((resolve) => {
+        writeFile(path.resolve(processPath, dist), json, (err) => {
           console.log(err);
           resolve();
         });
       });
       return pjson;
     });
-}
+};
 
-atomicBuilder.init = function (opt) {
-  var dist = opt.dist;
-  var src = opt.src;
-  var sample = opt.sample;
-  var p1 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/css", path.resolve(process_path, dist, "./css"), function (err) {
+atomicBuilder.init = (opt) => {
+  const dist = opt.dist;
+  const src = opt.src;
+  const sample = opt.sample;
+  const p1 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/css`, path.resolve(processPath, dist, './css'), (err) => {
       console.log(err);
       resolve();
     });
   });
-  var p2 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/js", path.resolve(process_path, dist, "./js"), function (err) {
+  const p2 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/js`, path.resolve(processPath, dist, './js'), () => {
       resolve();
     });
   });
-  var p3 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/images", path.resolve(process_path, dist, "./images"), function (err) {
+  const p3 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/images`, path.resolve(processPath, dist, './images'), () => {
       resolve();
     });
   });
-  var p4 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/index.html", path.resolve(process_path, dist, "./index.html"), function (err) {
+  const p4 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/index.html`, path.resolve(processPath, dist, './index.html'), () => {
       resolve();
     });
   });
-  var p5 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/ace", path.resolve(process_path, dist, "./ace"), function (err) {
+  const p5 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/ace`, path.resolve(processPath, dist, './ace'), () => {
       resolve();
     });
   });
-  var p6 = new Promise(function (resolve, reject) {
-    fs.copy(__dirname + "/config.js", path.resolve(process_path, dist, "./config.js"), function (err) {
+  const p6 = new Promise((resolve) => {
+    fs.copy(`${__dirname}/config.js`, path.resolve(processPath, dist, './config.js'), () => {
       resolve();
     });
   });
-  var p7 = new Promise(function (resolve, reject) {
+  const p7 = new Promise((resolve) => {
     if (sample) {
-      fs.copy(__dirname + "/components", path.resolve(process_path, src), function (err) {
+      fs.copy(`${__dirname}/components`, path.resolve(processPath, src), () => {
         resolve();
       });
     } else {
       resolve();
     }
   });
-  var p8 = new Promise(function (resolve, reject) {
+  const p8 = new Promise((resolve) => {
     if (sample) {
-      fs.copy(__dirname + "/resources/setting.json", path.resolve(process_path, dist, "./resources/setting.json"), function (err) {
+      fs.copy(`${__dirname}/resources/setting.json`, path.resolve(processPath, dist, './resources/setting.json'), () => {
         resolve();
       });
     } else {
@@ -177,6 +174,6 @@ atomicBuilder.init = function (opt) {
   });
 
   return Promise.all([p1, p2, p3, p4, p5, p6, p7, p8]);
-}
+};
 
 module.exports = atomicBuilder;
