215fb4b8f934cd90fe5059cf906b7a44d86e6950
steelydylan <ess_president@me.com>
2017-07-15 00:57:42 +0900

    added header and hero

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ atomicLab.init = (opt) => {
   const dist = opt.dist;
   const src = opt.src;
   const promiseArray = [
-    copyPromise(`${__dirname}/index.html`, path.resolve(processPath, dist, './index.html')),
+    copyPromise(`${__dirname}/demo.html`, path.resolve(processPath, dist, './index.html')),
     copyPromise(`${__dirname}/config.json`, path.resolve(processPath, dist, './config.json')),
     copyPromise(`${__dirname}/bundle.js`, path.resolve(processPath, dist, './bundle.js')),
     copyPromise(`${__dirname}/components.json`, path.resolve(processPath, dist, './components.json')),
